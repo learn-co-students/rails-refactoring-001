27e634cb4d811cb353adf47441df0bbfcdfd133d
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-10-07 18:33:57 -0400

    stubbed github api calls with vcr. replace controller tests with unit tests

--- app/models/github_connection.rb
+++ app/models/github_connection.rb
@@ -1,32 +1,44 @@
 class GithubConnection
-  attr_reader :orgs, :repos, :issues, :username, :token
-
-  def initialize(user)
-    @username = user.username
-    @token = user.token
+  attr_reader :username, :token, :orgs, :repos, :issues
+  
+  def initialize(auth_data)
+    @username = auth_data["username"]
+    @token = auth_data["token"]
   end
 
+  # GET request to get user's organizations; method called on instance in the organizations controller
   def get_organizations
-    orgs = json_parse("https://api.github.com/user/orgs", "login")
-  end
-
-  def get_repos(organization)
-    repos = json_parse("https://api.github.com/orgs/#{organization}/repos", "name")
-  end
-
-  def get_issues(organization, repo)
-    issues = json_parse("https://api.github.com/repos/#{organization}/#{repo}/issues")
-  end
-
-  def make_request(url, header)
-    request = Typhoeus::Request.new(url, header)
-    request.run
-  end
-
-  def json_parse(url, key = nil)
-    JSON.parse(make_request(url, headers: {Authorization: "token #{token}"}).body).map do |to_parse| 
-      key ? to_parse[key] : to_parse
+    request = Typhoeus::Request.new(
+      "https://api.github.com/user/orgs",
+      headers: {Authorization: "token #{token}"}
+    )
+    response = request.run
+    orgs = JSON.parse(response.body).map do |org| 
+      org["login"]
     end
   end
 
+  # GET request to get all of an organization's repos; method called on instance in the organizations controller
+  def get_repos(organization)
+    request = Typhoeus::Request.new(
+      "https://api.github.com/orgs/#{organization}/repos",
+      headers: {Authorization: "token #{token}"}
+      )
+    response = request.run
+    repos = JSON.parse(response.body).map do |repo|
+      repo["name"]
+    end
+  end
+
+  # GET request to get all of a repos issues; method called on instance in the repos controller
+  def get_issues(organization, repo)
+    request = Typhoeus::Request.new(
+      "https://api.github.com/repos/#{organization}/#{repo}/issues",
+      headers: {Authorization: "token #{token}"}
+      )
+    response = request.run
+    repos = JSON.parse(response.body).map do |issue|
+      issue
+    end
+  end
 end
